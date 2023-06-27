8472f24841cb19a0f7f984f83184339b91e5be9c
6Thanapat9 <135731506+6Thanapat9@users.noreply.github.com>
2023-06-27 15:35:25 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App(){
+  return(
+    <>
+      <div className='card'>
+        <Avatar />
+        <div className='data'>
+          <Intro />
+          <SkillList />
+    
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      <Skill skill='Ai' emoji='🤖' color='red' />
+      <Skill skill='Python' emoji='👍🏻' color='blue' />
+      <Skill skill='Html' emoji='👶🏻' color='yellow' />
+      <Skill skill='Css' emoji='🤏🏼' color='cyan' />
+
+    </div>
+
+  );
+}
+
+function Skill(props){
+  return(
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+
+    </div>
+
+  );
+}
+
+function Intro(){
+  return(
+    <div>
+      <h1>นายธนภัทร ปิยะวงษ์</h1>
+      <p>
+        นักศึกษาสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์ 
+        มหาวิทยาลัยอุบลราชธานี ชื่นชอลในการใช้คอมพิวเตอร์ การเขียนโปรเเกรม การเขียนเว็ป
+        งานอดิเรก ฟังเพลง อ่านหนังสือ และเล่นเกม
+      </p>
+
+    </div>
+
+  );
+}
+
+function Avatar() {
+  return <img className='avatar' src='22750.jpg' alt='My Avatar'></img>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +66,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
